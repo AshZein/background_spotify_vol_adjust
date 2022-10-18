fcbb245ca87b85934ae786a9a3bca2a61ecac463
AshZein <105140774+AshZein@users.noreply.github.com>
2022-10-17 20:45:25 -0400

    Add files via upload

---
+++ active_window.py
@@ -0,0 +1,17 @@
+import psutil
+import win32gui
+import win32process
+
+def active_window_process_name() -> str:
+    """Gets the active window in its application name format (so .exe form)
+
+    retrieved from:
+    https://stackoverflow.com/questions/14394513/win32gui-get-the-current-active-application-name
+    """
+    while True:
+        try:
+            pid = win32process.GetWindowThreadProcessId(win32gui.
+                                                        GetForegroundWindow())
+            return psutil.Process(pid[-1]).name()
+        except:
+            pass
